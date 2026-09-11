351e4eebecff55a6c4665e5d6a5162ce27246f1a
sofiver2023 <verdejasofia4@gmail.com>
2026-09-11 13:52:32 -0300

    feat(clientesService): agregar obtenerClientes, obtenerClientePorId y eliminarCliente

--- src/services/clientesService.js
+++ src/services/clientesService.js
@@ -3,15 +3,28 @@ import axios from "axios";
 const URL = "https://fakestoreapi.com/users";
 
 const crearCliente = async (cliente) => {
+  const respuesta = await axios.post(URL, cliente);
+  return respuesta.data;
+};
 
-    const respuesta = await axios.post(
-        URL,
-        cliente
-    );
+const obtenerClientes = async () => {
+  const respuesta = await axios.get(URL);
+  return respuesta.data;
+};
 
-    return respuesta.data;
+const obtenerClientePorId = async (id) => {
+  const respuesta = await axios.get(`${URL}/${id}`);
+  return respuesta.data;
+};
+
+const eliminarCliente = async (id) => {
+  const respuesta = await axios.delete(`${URL}/${id}`);
+  return respuesta.data;
 };
 
 export default {
-    crearCliente
+  crearCliente,
+  obtenerClientes,
+  obtenerClientePorId,
+  eliminarCliente,
 };
