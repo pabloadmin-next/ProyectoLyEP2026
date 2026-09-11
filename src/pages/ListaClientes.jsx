fcdd441ff8ee39480fe6e713d38795baa7b9aaba
Pablo Vale <pablovaleuniversidad@gmail.com>
2026-09-11 17:05:13 -0300

    agrega filtro por nombre

--- src/pages/ListaClientes.jsx
+++ src/pages/ListaClientes.jsx
@@ -28,17 +28,17 @@ const ListaClientes = () => {
   }, []);
 
   const clientesFiltrados = clientes.filter(
-  (cliente) =>
-    cliente.name.firstname
-      .toLowerCase()
-      .includes(busqueda.toLowerCase()) ||
-    cliente.name.lastname
-      .toLowerCase()
-      .includes(busqueda.toLowerCase()) ||
-    cliente.address.city
-      .toLowerCase()
-      .includes(busqueda.toLowerCase())
-);
+    (cliente) =>
+      cliente.name.firstname
+        .toLowerCase()
+        .includes(busqueda.toLowerCase()) ||
+      cliente.name.lastname
+        .toLowerCase()
+        .includes(busqueda.toLowerCase()) ||
+      cliente.address.city
+        .toLowerCase()
+        .includes(busqueda.toLowerCase())
+  );
 
   if (loading) {
     return <h2>Cargando clientes...</h2>;
